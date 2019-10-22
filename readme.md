8a4f8e1927a105e47567dba0c0fd8dfb4c63a156
Mitali Shah <mitalishaleenshah@gmail.com>
2019-10-22 16:10:39 -0700

    added reset functionality to change customtext = '' and fontsize = 20, added readme file with live project link

---
+++ readme.md
@@ -0,0 +1,21 @@
+## About this project: ##
+
+### This is a pre-work project for [Chingu](https://www.chingu.io/) Voyage-13, More information about the requirement is [here](https://github.com/chingu-voyages/voyage-prework-tier1-favfonts) ###
+
+## Technologies used: ##
+
+- HTML, CSS and Javascript
+
+## Functionalities ##
+
+- Text typed into the custom text (type something) box should immediately change the sample text displayed in each font card
+- The sample text should return to the default sample if the input box (type something) no longer has any input (ex. input == "")
+- Font size chooser should have at least four sizes and should immediately change the sample text font size in each font card
+- Implement the clickable 'reset' icon on the far right of the major navigation; it should make the page appear as if the user reloaded the page (do not actually reload the page)
+
+
+## Project Link: ##
+
+### Click [here](https://mitalishah.github.io/v13-prework/) to view the live page ###
+
+
--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-//change custom text
-const customInput = document.getElementById("custom-text");
+//add custom text and display on font preview elemnent
+let customInput = document.getElementById("custom-text");
 
 function changeCustomText(e){
-  
+
   let fontCardPreviewTexts = document.querySelectorAll(".font-preview p");
 
   fontCardPreviewTexts.forEach(fontCardPreviewText => {
@@ -23,19 +23,45 @@ function changeCustomText(e){
 customInput.oninput = changeCustomText;
 
 
-//change font size
+//change font size for all font preview text
 
 let fontSelect = document.getElementById("font-size");
+let fontCards = document.querySelectorAll(".font-preview p");
 
 function changeFontSize(e){
-  let fontCards = document.querySelectorAll(".font-preview p");
 
-  console.log(e.target.value);
+  fontCards.forEach(fontcard =>{
+    fontcard.style.fontSize = `${e.target.value}px`;
+  })
 
-  for (let i=0; i < fontCards.length; i++) {
-    fontCards[i].style.fontSize = `${e.target.value}px`;  
-  }
 }
 
 fontSelect.onchange = changeFontSize;
+
+//reset button
+const resetButton = document.getElementById("reset");
+
+function reset(){
+
+  let resetCustomText = {
+    target: {
+      value: ""
+    }
+  }
+
+  let resetFont = {
+    target: {
+      value: 20
+    }
+  }  
+
+  customInput.value = "";
+  fontSelect.value = "20";
+  changeCustomText(resetCustomText);
+  changeFontSize(resetFont);
+}
+
+resetButton.onclick = reset;
+
+
 
